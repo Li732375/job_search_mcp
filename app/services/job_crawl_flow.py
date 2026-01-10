6b5273e8bf2ca64c6796f03e9d66f3612b490e12
Li732375 <lee732375@gmail.com>
2026-01-10 10:26:34 +0800

    Refactor error handling to check for existence of error_message.json before loading errors

--- app/services/job_crawl_flow.py
+++ app/services/job_crawl_flow.py
@@ -3,6 +3,7 @@ from .spyE04 import SpyE04
 
 import time
 import json
+import os
 
 
 def crawl_E04_jobs(spider: SpyE04) -> None:
@@ -26,9 +27,10 @@ def crawl_E04_jobs(spider: SpyE04) -> None:
     spider.fetch_jobs_and_write_csv(job_ids, output_file)
 
     # 依是否有錯誤紀錄，調整輸出結果
-    with open('error_message.json', 'r', encoding='utf-8-sig') as f:
-        errors = json.load(f)
-    if errors:
-        print(f"\n任務完成！\n資料已寫入 {output_file}\n[錯誤] 請查看 error_message.json")
-    else:
-        print(f"\n任務完成！\n資料已寫入 {output_file}")
+    if os.path.exists('error_message.json'):
+        with open('error_message.json', 'r', encoding='utf-8-sig') as f:
+            errors = json.load(f)
+        if len(errors) > 0:
+            print(f"\n任務完成！\n資料已寫入 {output_file}\n[錯誤] 請查看 error_message.json")
+        else:
+            print(f"\n任務完成！\n資料已寫入 {output_file}")
