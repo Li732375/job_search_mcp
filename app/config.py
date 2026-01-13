28e3aa6f5422ec625abab79f849d4f9d3056b0e8
Li732375 <lee732375@gmail.com>
2026-01-13 17:22:55 +0800

    Remove unused field names and job field mapping from config.py

--- app/config.py
+++ app/config.py
@@ -14,37 +14,7 @@ E04_MUL_FILTER_PARAMS = {
     'jobcat': '2007001004,2007001020',
 }
 
-# 欄位名稱順序
-FIELD_NAMES_ORDER = [
-    '更新日期', '工作型態', '工作時段', '薪資類型', '最低薪資',
-    '最高薪資', '職缺名稱', '學歷', '工作經驗', '工作縣市',
-    '工作里區', '工作地址', '公司名稱', '職缺描述', '其他描述',
-    '擅長要求', '證照', '駕駛執照', '出差', '104 職缺網址', 
-    '公司產業類別', '法定福利'
-]
+### 以下變數不建議改動
 
-# 職缺欄位對應表
-JOB_FIELD_MAPPING = {
-    '更新日期': 'posted_date',
-    '工作型態': 'work_type',
-    '工作時段': 'work_shift',
-    '薪資類型': 'salary_type',
-    '最低薪資': 'salary_min',
-    '最高薪資': 'salary_max',
-    '職缺名稱': 'job_name',
-    '學歷': 'education',
-    '工作經驗': 'experience',
-    '工作縣市': 'address_area',
-    '工作里區': 'job_area',
-    '工作地址': 'address_detail',
-    '公司名稱': 'company_name',
-    '職缺描述': 'job_description',
-    '其他描述': 'other_description',
-    '擅長要求': 'specialty',
-    '證照': 'certificate',
-    '駕駛執照': 'driver_license',
-    '出差': 'business_trip',
-    '104 職缺網址': 'job_url',
-    '公司產業類別': 'industry',
-    '法定福利': 'legal_welfare'
-}
+# 職缺資料庫路徑
+JOB_DATA_LOCAL_URL = 'app/database/job_data.db'
